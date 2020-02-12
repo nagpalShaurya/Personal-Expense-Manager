956cb769b200ba1715f2009f5fc129877ed7053e
nagpalShaurya <shauryanagpal1998@gmail.com>
2020-02-12 20:06:25 +0530

    updated ui

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,7 @@ class MyHomePage extends StatelessWidget {
             child: Text('CHARTS!'),           //method 2 to do the same....
           ),
         ),
+
         Column(children: transactions.map((tx) {
           return Card (
             child: Row(children: <Widget>[
@@ -72,21 +73,38 @@ class MyHomePage extends StatelessWidget {
                       horizontal: 15,
                       ),
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
+                        border: Border.all(
+                        color: Colors.purple,
                         width:2,
                         ),
                       ),
                     padding: EdgeInsets.all(10),
                     child: Text(tx.amount.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                      ),
                     ),
                     ),
-                  Column(children: <Widget>[
-                    Text(tx.title),
-                    Text(tx.date.toString(),),
-                  ],),   
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                    Text(
+                      tx.title, 
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    Text(tx.date.toString(),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                  ),   
             ],),
             );
+
         }).toList()),
       ],
       ),
